f2f666fa2de88bcd4d488955411829b7d98935b1
tanya <tanyapoojari2004@gmail.com>
2026-06-16 16:13:31 +0530

    Update API URL for production

--- notes-website-frontend/src/services/api.js
+++ notes-website-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ 
-  baseURL: 'http://localhost:5000/api'
+const API = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
 // Request interceptor to add auth token
